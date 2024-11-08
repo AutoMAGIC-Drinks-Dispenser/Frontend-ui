769b7d8021f5a69141883acba1d979717d5b7d28
SimonEifer <202306749@post.au.dk>
2024-11-08 11:22:09 +0100

    fixes error in extra_menu.tsx

--- src/components/extra_menu.tsx
+++ src/components/extra_menu.tsx
@@ -1,14 +1,8 @@
-interface ExtraMenuProps {
-  userId: string;
-}
-
-export const ExtraMenu: React.FC<ExtraMenuProps> = ({ userId }) => {
-  const name = "Big Spejlæg Man";
-
+export const ExtraMenu: React.FC = () => {
   return (
     <div className="bg-yellow-400 text-center py-4 w-full flex justify-center space-x-24">
       <div className="flex justify-item-center space-x-4">
-        <span className="text-black">User: {(userId = name)}</span>
+        <span className="text-black">User: </span>
       </div>
       <button className="bg-stone-400 text-white px-6 py-2 rounded-md hover:bg-stone-700 focus:outline-none w-56">
         Påfyld væskebeholdere
