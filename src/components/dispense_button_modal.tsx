7e53c638e9e35f36865723c616f97a8d692ff562
Simon <202306749@post.au.dk>
2024-11-10 17:33:04 +0100

    adds minor changes to dispensebutton component and modal

---
+++ src/components/dispense_button_modal.tsx
@@ -0,0 +1,28 @@
+interface PopupProps {
+  onClose: () => void;
+  onStart: () => void;
+}
+
+export const SpejlaegPopupModal: React.FC<PopupProps> = ({ onClose, onStart }) => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="bg-white p-8 rounded-md shadow-lg text-center">
+        <h2 className="text-2xl font-bold mb-4">Bekræft Dispensing</h2>
+        <div className="flex justify-around mt-8">
+          <button
+            onClick={onStart}
+            className="bg-green-500 text-white px-6 py-2 rounded-md hover:bg-green-700 focus:outline-none"
+          >
+            Start
+          </button>
+          <button
+            onClick={onClose}
+            className="bg-red-500 text-white px-6 py-2 rounded-md hover:bg-red-700 focus:outline-none"
+          >
+            Annullér
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/extra_menu.tsx
+++ src/components/extra_menu.tsx
@@ -2,7 +2,7 @@ export const ExtraMenu: React.FC = () => {
   return (
     <div className="bg-yellow-400 text-center py-4 w-full flex justify-center space-x-24">
       <div className="flex justify-item-center space-x-4">
-        <span className="text-black">User: </span>
+        <span className="text-black">User: Placeholder name</span>
       </div>
       <button className="bg-stone-400 text-white px-6 py-2 rounded-md hover:bg-stone-700 focus:outline-none w-56">
         Påfyld væskebeholdere
@@ -10,6 +10,10 @@ export const ExtraMenu: React.FC = () => {
       <button className="bg-stone-400 text-white px-6 py-2 rounded-md hover:bg-stone-700 focus:outline-none w-56">
         Rengør system
       </button>
+      <button className="bg-stone-400 text-white px-6 py-2 rounded-md hover:bg-stone-700 focus:outline-none w-56">
+        Afspil jingle
+      </button>
     </div>
+    
   );
 };
