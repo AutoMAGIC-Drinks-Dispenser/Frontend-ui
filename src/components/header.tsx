f49aa53dbea8b62e84680c5287ff59d29c28eb53
Simon <202306749@post.au.dk>
2024-11-12 13:06:04 +0100

    adds modals for påfyld væskebeholdere and rengør system buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,9 +4,6 @@ import { ExtraMenu } from "./menu/extra_menu";
 export const HeaderComponent: React.FC = () => {
   return (
     <div className="bg-stone-200 text-center py-4 w-full border-b-2 border-black flex items-center">
-      <h1 className="text-2xl font-extrabold text-black px-12 flex-grow whitespace-nowrap">
-        AutoMAGIC Spejlægdispenser
-      </h1>
       <ExtraMenu />
     </div>
   );
--- src/components/menu/extra_menu.tsx
+++ src/components/menu/extra_menu.tsx
@@ -1,15 +1,13 @@
 import { AddUser } from "./add_user";
+import { CleaningButton } from "./clean_modal";
+import { RefillButton } from "./refill_modal";
 
 export const ExtraMenu: React.FC = () => {
   return (
-    <div className="bg-stone-200 text-center py-4 w-full flex justify-end space-x-8 mr-4">
+    <div className="bg-stone-200 text-center py-4 w-full flex justify-center space-x-20">
       <AddUser />
-      <button className="bg-zinc-800 text-white px-6 py-2 rounded-md hover:bg-zinc-950 focus:outline-none w-56">
-        Påfyld væskebeholdere
-      </button>
-      <button className="bg-zinc-800 text-white px-6 py-2 rounded-md hover:bg-zinc-950 focus:outline-none w-56">
-        Rengør system
-      </button>
+      <RefillButton />
+      <CleaningButton />
       <button className="bg-zinc-800 text-white px-6 py-2 rounded-md hover:bg-zinc-950 focus:outline-none w-56">
         Afspil jingle
       </button>
