f20219ca91496ccafc0db640c477fc60e0697e4f
Simon <202306749@post.au.dk>
2024-11-19 12:04:11 +0100

    fixes buttons

--- src/components/dispense_button.tsx
+++ src/components/dispense_button.tsx
@@ -22,7 +22,7 @@ export const SpejlaegButtonComponent: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen overflow-hidden">
+    <div className="flex items-center justify-center p-4">
       <div className="flex space-x-12">
         <button
           onClick={handleSingleButtonClick}
