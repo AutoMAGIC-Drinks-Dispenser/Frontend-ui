5ae7393322397aac80c5aac28408c638b38164de
Simon <202306749@post.au.dk>
2024-11-19 11:36:05 +0100

    test

--- src/components/main_menu.tsx
+++ src/components/main_menu.tsx
@@ -3,13 +3,19 @@ import { HeaderComponent } from "./header.tsx";
 
 export const MainMenu: React.FC = () => {
   return (
-    <div>
-      <div>
+    <div style={{ width: "800px", height: "480px", overflow: "hidden" }}>
+      <div style={{ textAlign: "center" }}>
         <HeaderComponent />
       </div>
-      <img className="p-4 w-auto h-auto" src="../../src/Images/blub3.png" />
-      <SpejlaegButtonComponent />
-      <img />
+      <img
+        className="p-4 w-auto h-auto"
+        src="../../src/Images/blub3.png"
+        style={{ maxWidth: "100%", maxHeight: "50%" }}
+      />
+      <div style={{ textAlign: "center", marginTop: "10px" }}>
+        <SpejlaegButtonComponent />
+      </div>
+      <img style={{ display: "none" }} />
     </div>
   );
 };
