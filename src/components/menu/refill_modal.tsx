914858f4acedfab5db14e9ff8721c46a1368d5bd
Jeppe <Jeppe@fristed.dk>
2024-12-03 09:31:38 +0100

    updstes the design of the front page - and changes the buttons to modals

--- src/components/menu/refill_modal.tsx
+++ src/components/menu/refill_modal.tsx
@@ -4,13 +4,13 @@ interface PopupProps {
   onConfirm: () => void;
 }
 
-export const RefillPopupModal: React.FC<PopupProps> = ({ onConfirm }) => {
+export const RefillPremixPopupModal: React.FC<PopupProps> = ({ onConfirm }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-8 rounded-md shadow-lg text-center">
         <h2 className="text-2xl font-bold mb-4">Bekræft påfyldning</h2>
         <p className="text-gray-700 mb-6">
-          Sørg for, at alt er klar, før du bekræfter.
+          Sørg for, at alt er klar, og at Spejlægmixen er opfyldt, før du bekræfter.
         </p>
         <button
           onClick={onConfirm}
@@ -23,24 +23,23 @@ export const RefillPopupModal: React.FC<PopupProps> = ({ onConfirm }) => {
   );
 };
 
-export const RefillButton: React.FC = () => {
-  const [isPopupOpen, setPopupOpen] = useState(false);
-
-  const handleOpenPopup = () => setPopupOpen(true);
-  const handleConfirm = () => {
-    setPopupOpen(false);
-    console.log("Påfyldning bekræftet!");
-  };
-
+export const RefillPostmixPopupModal: React.FC<PopupProps> = ({ onConfirm }) => {
   return (
-    <div>
-      <button
-        className="bg-zinc-800 text-xs text-white px-6 py-2 rounded-md hover:bg-zinc-950 focus:outline-none w-32"
-        onClick={handleOpenPopup}
-      >
-        Påfyld væskebeholdere
-      </button>
-      {isPopupOpen && <RefillPopupModal onConfirm={handleConfirm} />}
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="bg-white p-8 rounded-md shadow-lg text-center">
+        <h2 className="text-2xl font-bold mb-4">Bekræft påfyldning</h2>
+        <p className="text-gray-700 mb-6">
+          Sørg for, at alt er klar, og at faxekondi'en er påfyldt før du bekræfter.
+        </p>
+        <button
+          onClick={onConfirm}
+          className="bg-blue-400 text-white px-6 py-2 rounded-md hover:bg-blue-500 focus:outline-none"
+        >
+          Bekræft påfyldning
+        </button>
+      </div>
     </div>
   );
 };
+
+
