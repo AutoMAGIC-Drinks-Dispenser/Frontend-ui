0a900aecfce79cc8f84d7b55c1d4f18ef5658703
Jeppe <Jeppe@fristed.dk>
2024-12-09 21:17:51 +0100

    fetch this shit

--- src/components/menu/extra_menu.tsx
+++ src/components/menu/extra_menu.tsx
@@ -2,12 +2,14 @@
 import { AddUser } from "./add_user";
 import { CleaningButton } from "./clean_modal";
 import { RefillButton } from "./refill_modal";
+import ArduinoControl from "./../connectArduinoButton"
 
 export const ExtraMenu: React.FC = () => {
   return (
     <div className="bg-stone-200 text-center py-4 w-screen flex justify-center space-x-5">
       <AddUser />
       <RefillButton />
+      <ArduinoControl />
       <CleaningButton />
       <button className="bg-zinc-800 text-xs text-white px-6 py-2 rounded-md hover:bg-zinc-950 focus:outline-none w-32">
         Afspil jingle
