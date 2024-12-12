70a0145148b851223fb9ae74e6a56ab70665dc51
ChristopherDolan <202305666@post.au.dk>
2024-12-12 09:54:42 +0100

    changing cleaning modal structure

--- src/components/menu/clean_modal.tsx
+++ src/components/menu/clean_modal.tsx
@@ -1,44 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { sendDataToArduino } from "./../communication/web_serial_com";
+import { PopupProps } from "../dispense_button_modal";
 
 
-interface TimerPopupProps {
-  onClose: () => void;
-  duration: number; // Timer duration in seconds
-}
-
-export const CleaningPopupModal: React.FC<TimerPopupProps> = ({
+export const CleaningPopupModal: React.FC<PopupProps> = ({
   onClose,
-  duration,
+  onStart,
 }) => {
-  const [timeLeft, setTimeLeft] = useState(duration);
-
-  useEffect(() => {
-    if (timeLeft > 0) {
-      const timer = setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
-      return () => clearTimeout(timer); // Cleanup on unmount or new timer start
-    } else {
-      onClose(); // Close popup when timer reaches 0
-    }
-  }, [timeLeft, onClose]);
-
-  const progress = ((duration - timeLeft) / duration) * 100;
-
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-8 rounded-md shadow-lg text-center">
-        <h2 className="text-2xl font-bold mb-4">Skyller system</h2>
-        <p className="text-gray-700 mb-6">
-          Rengøring er i gang, vent venligst...
-        </p>
-        <div className="text-xl font-bold text-blue-500 mb-4">
-          Tid tilbage: {timeLeft} sekunder
-        </div>
-        <div className="w-full bg-gray-300 rounded-full h-4">
-          <div
-            className="bg-blue-400 h-4 rounded-full"
-            style={{ width: `${progress}%` }}
-          ></div>
+        <h2 className="text-2xl font-bold mb-4">Bekræft Rengøring</h2>
+        <div className="flex justify-around mt-8">
+          <button
+            onClick={onStart}
+            className="bg-blue-400 text-white px-6 py-2 rounded-md hover:bg-blue-500 focus:outline-none"
+          >
+            Start
+          </button>
+          <button
+            onClick={onClose}
+            className="bg-red-500 text-white px-6 py-2 rounded-md hover:bg-red-700 focus:outline-none"
+          >
+            Annullér
+          </button>
         </div>
       </div>
     </div>
@@ -82,7 +67,11 @@ export const CleaningButton: React.FC = () => {
         Rengør system
       </button>
       {showPopup && (
-        <CleaningPopupModal onClose={handleClosePopup} duration={30} />
+        <CleaningPopupModal 
+          onClose={handleClosePopup} 
+          onStart={handleStart} 
+          err={""}
+        />
       )}
     </div>
   );
