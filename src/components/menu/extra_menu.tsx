715c4642288cd7add13977254657a947fefc75f8
ChristopherDolan <202305666@post.au.dk>
2024-12-12 11:10:00 +0100

    removes button

--- src/components/menu/extra_menu.tsx
+++ src/components/menu/extra_menu.tsx
@@ -6,9 +6,6 @@ export const ExtraMenu: React.FC = () => {
     <div className="bg-stone-200 text-center py-4 w-screen flex justify-center space-x-5">
       <AddUser />
       <CleaningButton />
-      <button className="bg-zinc-800 text-xs text-white px-6 py-2 rounded-md hover:bg-zinc-950 focus:outline-none w-32">
-        Afspil jingle
-      </button>
     </div>
   );
 };
